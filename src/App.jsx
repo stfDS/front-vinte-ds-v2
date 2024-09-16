bd32bb5e5918b31b73a8bb7acacf1bc0549bfe9d
stfDS <dossantos.stefane@gmail.com>
2024-09-16 20:49:46 +0200

    add user and offers routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import "./App.css"
 import Home from "./pages/Home/Home"
 import Header from "./components/Header/Header"
 import Footer from "./components/Footer/Footer"
+import { DataProvider } from "./context/data.context"
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <Footer />
+      <DataProvider>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </DataProvider>
     </BrowserRouter>
   )
 }
---
+++ src/components/HomeArticle/HomeArticle.jsx
@@ -0,0 +1,30 @@
+import "./homeArticle.css"
+import { Link } from "react-router-dom"
+
+const HomeArticle = ({ offer }) => {
+  return (
+    <article className="home-article">
+      {/* <Link to={`/offer/${offer._id}`} className="home-article-link"> */}
+      <Link className="home-article-link">
+        <div className="home-offer-pic">
+          {offer.product_pictures[0].secure_url && (
+            <img src={offer.product_pictures[0].secure_url} alt="" />
+          )}
+        </div>
+        <div className="home-product-details">
+          <div>
+            <p>{offer.product_details[0].MARQUE}</p>
+          </div>
+          <div>
+            <p>{offer.product_details[1].TAILLE}</p>
+          </div>
+          <div>
+            <p>{offer.product_price} €</p>
+          </div>
+        </div>
+      </Link>
+    </article>
+  )
+}
+
+export default HomeArticle
